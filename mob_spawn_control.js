6edc371c946ed67f883a399d5a096b55e8c8e394
Liopyu <mistakensound@gmail.com>
2023-11-26 23:30:03 -0500

    Added Equipment Control

--- mob_spawn_control.js
+++ mob_spawn_control.js
@@ -1,3 +1,12 @@
+/**
+ * Spawn Control Script by Liopyu
+ * GitHub Repo: https://github.com/liopyu/spawn_control
+ * - A KubeJS script designed for advanced mob spawning control in Minecraft.
+ * - Allows precise configuration for individual mob types based on light levels, biomes, dimensions, and more.
+ * - For the latest updates and discussions, visit the GitHub repository.
+ */
+
+
 // Define the path to the configuration file
 const configPath = 'config/spawn-control-config.json';
 
@@ -14,15 +23,51 @@ function isInArray(value, array) {
     return array.indexOf(value) !== -1;
 }
 
+// Function to equip items based on chances specified in the JSON file
+function equipItems(entity, equipment) {
+    Object.entries(equipment).forEach(([slotName, entries]) => {
+        const slotMap = {
+            helmet: 5,
+            chestplate: 4,
+            leggings: 3,
+            boots: 2,
+            offhand: 1,
+            mainhand: 0,
+        };
+
+        // Ensure slotName is lowercase for case-insensitivity
+        const lowerSlotName = slotName.toLowerCase();
+        const slot = slotMap[lowerSlotName];
+
+        if (slot !== undefined) {
+            // Random value to determine which item to choose
+            const randomValue = Math.random();
+            let cumulativeChance = 0;
+
+            // Iterate through options to find the chosen one
+            for (const [item, chance] of entries) {
+                cumulativeChance += chance;
+                if (randomValue <= cumulativeChance) {
+                    entity.setItemSlot(slot, item);
+                    break; // Stop iterating once an option is chosen
+                }
+            }
+        } else {
+            console.log(`Invalid slot name: ${slotName}`);
+        }
+    });
+}
+
 // Register the function to handle the mob spawning event
 EntityEvents.spawned(event => {
     // Define constants
     const entity = event.entity;
-    
+
     // Skip the logic if the entity is a player
     if (entity.isPlayer()) {
         return;
     }
+
     // Cancel the event if the entity type is not in allowedMobs
     if (!isInArray(entity.type, allowedMobs)) {
         if (allowAnyMobSpawn) {
@@ -43,7 +88,7 @@ EntityEvents.spawned(event => {
 
     const biomeId = entity.block.biomeId;
     const blockLightLevel = entity.block.getLight();
-    const daytime = event.level.getDayTime()
+    const daytime = event.level.getDayTime();
 
     // Normalize the biomeId and the whitelisted biomes for case insensitivity
     const normalizedBiomeId = biomeId.toString().toLowerCase();
@@ -68,7 +113,10 @@ EntityEvents.spawned(event => {
             (mobConditions.dayOrNight === 'night' && (daytime > 13000 || daytime === 0)) ||
             mobConditions.dayOrNight === 'both')
     ) {
-        // Do nothing, let the entity spawn
+        // Equip items based on chances specified in the JSON file
+        if (mobConditions.equipment) {
+            equipItems(entity, mobConditions.equipment);
+        }
     } else {
         event.cancel(); // Cancel the event if any of the conditions are not met
     }
